2c3765d4d481941b3328c73312463d0a7d5ede81
Lukasz Gryczewski <luka.gryczewski@gmail.com>
2023-01-09 03:23:43 +0100

    Form update Artist

--- resources/views/artist/show.blade.php
+++ resources/views/artist/show.blade.php
@@ -4,5 +4,15 @@
 
 @section('content')
     <h1>{{ $artist->firstname }} {{ $artist->lastname }}</h1>
+
+    <h2>List des types</h2>
+    <ul>
+        @foreach($artist->types as $type)
+            <li>{{$type->type}}</li>
+        @endforeach
+    </ul>
+
+    <div><a href="{{ route('artist.edit',$artist->id) }}">Modifer</a></a></div>
+    
     <nav><a href="{{ route('artist.index') }}">Retour à l'index</a></nav>
 @endsection
