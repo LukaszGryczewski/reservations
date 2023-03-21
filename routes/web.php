cde8ae09e0624d3bda58962fab08d4642f02f463
Lukasz Gryczewski <luka.gryczewski@gmail.com>
2023-03-21 13:14:11 +0100

    Created Controller Show , templates and routes

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\TypeController;
 use App\Http\Controllers\LocalityController;
 use App\Http\Controllers\LocationController;
 use App\Http\Controllers\RoleController;
+use App\Http\Controllers\ShowController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -45,6 +46,10 @@ Route::get('/locality/{id}',[LocalityController::class,'show'])
 Route::get('location', [LocationController::class, 'index'])->name('location.index');
 Route::get('location/{id}', [LocationController::class, 'show'])
 ->where('id', '[0-9]+')->name('location.show');
+
+Route::get('/show', [ShowController::class, 'index'])->name('show.index');
+Route::get('/show/{id}', [ShowController::class, 'show'])
+->where('id', '[0-9]+')->name('show.show');
 
 Route::get('/role',[RoleController::class,'index'])
     ->name('role.index');
